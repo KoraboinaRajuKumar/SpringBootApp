7be48778e8b4b55ffdc24b03f6a5500e7be9f34b
unknown <rajujava0355@gmail.com.com>
2023-04-03 20:28:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/rest/RestApi.java
@@ -0,0 +1,15 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApi {
+
+	@RequestMapping("/springboot")
+	public String msg() {
+
+		return "hello springboot docker app";
+	}
+
+}
